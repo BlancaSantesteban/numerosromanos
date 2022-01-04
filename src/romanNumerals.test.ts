21d68111b6ab5e6ac619d7523a64f73c1345231e
Blanca Santesteban <blanca.santesteban@biko2.com>
2022-01-04 13:22:39 +0100

    style: añade salto de linea

--- src/romanNumerals.test.ts
+++ src/romanNumerals.test.ts
@@ -3,6 +3,7 @@ import { romanNumeral } from './romanNumeral';
 describe('Roman numerals', () => {
   it('Valor 1 devuelve I', () => {
     const result = romanNumeral(1);
+
     expect(result).toBe('I');
   });
 });
