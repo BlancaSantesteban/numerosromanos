b6f9598065d1130e19d169ebb053aafa83153710
Blanca Santesteban <blanca.santesteban@biko2.com>
2022-01-04 13:20:52 +0100

    feat: Valor 1 devuelve I

--- src/foo.test.ts
+++
@@ -1,7 +0,0 @@
-import { foo } from './foo';
-
-describe('Foo [REMOVE THIS SUITE BEFORE WORKING]', () => {
-  it('is foo', () => {
-    expect(foo()).toBe('foo');
-  });
-});
---
+++ src/romanNumerals.test.ts
@@ -0,0 +1,10 @@
+describe('Roman numerals', () => {
+  it('Valor 1 devuelve I', () => {
+    const result = romanNumeral(1);
+    expect(result).toBe('I');
+  });
+});
+
+function romanNumeral(arg0: number) {
+  return 'I';
+}
