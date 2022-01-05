a23b08d56d3b2bb5f2e645db967343e50561c9a3
Blanca Santesteban <blanca.santesteban@biko2.com>
2022-01-05 13:53:16 +0100

    refactor: crea array

--- src/romanNumeral.ts
+++ src/romanNumeral.ts
@@ -1,4 +1,44 @@
 export function romanNumeral(arabic: number) {
+  const arabicOneLetter = [
+    {
+      arabic: 1,
+      roman: 'I',
+    },
+    {
+      arabic: 5,
+      roman: 'V',
+    },
+    {
+      arabic: 10,
+      roman: 'X',
+    },
+    {
+      arabic: 50,
+      roman: 'L',
+    },
+    {
+      arabic: 100,
+      roman: 'C',
+    },
+    {
+      arabic: 500,
+      roman: 'D',
+    },
+    {
+      arabic: 1000,
+      roman: 'M',
+    },
+  ];
+
+  const value = arabicOneLetter.find(element => {
+    if (element.arabic === arabic) {
+      return element.roman;
+    }
+  });
+  if (value) {
+    return value.roman;
+  }
+
   if (arabic === 2) {
     return 'II';
   }
@@ -8,29 +48,4 @@ export function romanNumeral(arabic: number) {
   if (arabic === 11) {
     return 'XI';
   }
-  if (arabic === 1000) {
-    return 'M';
-  }
-
-  if (arabic === 500) {
-    return 'D';
-  }
-
-  if (arabic === 100) {
-    return 'C';
-  }
-
-  if (arabic === 50) {
-    return 'L';
-  }
-
-  if (arabic === 10) {
-    return 'X';
-  }
-
-  if (arabic === 5) {
-    return 'V';
-  }
-
-  return 'I';
 }
